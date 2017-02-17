3754af1d5ad8e518aa4d435d0636217332a307f8
Gaetan Marecat <gaetan.marecat@epsi.fr>
2017-02-17 08:40:33 +0100

    mail de test

---
+++ application/src/AppBundle/Controller/Rest/TestRestController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Controller\Rest;
+
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Util\Codes;
+
+class TestRestController extends ParentRestController
+{
+
+    /**
+     * Test l'envoi de mail
+     *
+     * @ApiDoc(
+     * section = "Test",
+     *  output={"class"="Response"},
+     *  statusCodes={
+     *      200="Returned when successful",
+     *      500="Returned when error occured"
+     *  }
+     * )
+     *
+     * @param $to
+     * @return Response
+     */
+    public function getAllListesDiffusionAction($to)
+    {
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Hello World')
+            ->setFrom($this->container->getParameter("mailer_user"))
+            ->setTo($to)
+            ->setBody(
+                $this->renderView('@AppBundle/Resources/views/email/test.html.twig'
+                ),
+                'text/html'
+            )
+        ;
+        $this->get('mailer')->send($message);
+
+        return $this->view("Envoyé", Codes::HTTP_OK);
+    }
+
+}
