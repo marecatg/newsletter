6df1d500fae4d7af48047098da79668d152a22db
Gaetan Marecat <gaetan.marecat@epsi.fr>
2017-03-20 21:25:12 +0100

    registration

--- application/src/AppBundle/Controller/RegistrationController.php
+++ application/src/AppBundle/Controller/RegistrationController.php
@@ -37,7 +37,7 @@ class RegistrationController extends Controller
 
             // 4) save the User!
             $em = $this->getDoctrine()->getManager();
-            
+
             $em->persist($user);
             $em->flush();
 
